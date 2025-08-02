637868b288a0e351650e83e94c38c93f3d12fb3e
jbergandi <jbergandi@ecosistemas.com.ar>
2025-08-02 00:49:44 -0300

    fix: se repara api de Gemini

--- src/main/java/com/jlb/GeminiProvider.java
+++ src/main/java/com/jlb/GeminiProvider.java
@@ -8,7 +8,13 @@ import java.io.IOException;
 
 public class GeminiProvider implements IAProvider {
     private static final String GEMINI_API_KEY = System.getenv("GEMINI_API_KEY");
-    private static final String ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + GEMINI_API_KEY;
+    private static final String GEMINI_MODEL = "gemini-1.5-pro-latest";
+    private static final String ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/" + GEMINI_MODEL + ":generateContent";
+    private final boolean debug;
+
+    public GeminiProvider(boolean debug) {
+        this.debug = debug;
+    }
 
     @Override
     public String generarEscenario(String descripcionNueva, String contextoGherkin) throws IOException {
@@ -18,13 +24,19 @@ public class GeminiProvider implements IAProvider {
                 .put("contents", new JSONArray()
                         .put(new JSONObject()
                                 .put("parts", new JSONArray()
-                                        .put(new JSONObject().put("text", "Escenarios existentes:\n" + contextoGherkin + "\n\nGenerá un escenario Gherkin para: " + descripcionNueva))
+                                        .put(new JSONObject().put("text", "Escenarios existentes:\n" + contextoGherkin +"\nSos un generador de " +
+                                                "escenarios de pruebas automatizadas en lenguaje Gherkin. Tiene un listado de escenarios existentes " +
+                                                "en el proyecto para conocer las sentencias existentes y las practicas del mismo. Siempre que puedas " +
+                                                "reutiliza las frases que ya existen en los otros escenarios No expliques nada, solo generá el " +
+                                                "escenario, en base a esta consigna: " + descripcionNueva))
                                 )
                         )
                 );
 
         Request request = new Request.Builder()
                 .url(ENDPOINT)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("X-goog-api-key", GEMINI_API_KEY)
                 .post(RequestBody.create(requestBody.toString(), MediaType.parse("application/json")))
                 .build();
 
